75d6cea4ca6e978bbaeacd6ea754584ed991ac6a
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-17 09:56:09 -0500

    Proyecto inicial

---
+++ src/main/java/com/nb/springbootmicroserviceeurekaserver/SpringbootMicroserviceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceEurekaServerApplication.class, args);
+    }
+
+}
